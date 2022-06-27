731ed1bc88f8dbf3bf125d89035be3345d4dd8bd
shandawei <shandawei@me.com>
2022-06-27 18:12:08 +0800

    优化接口

--- script.js
+++ script.js
@@ -1,84 +1,61 @@
+// !第一步、获取当前页面URL签名信息
+// !第二步、进行config配置
+// !第三步、获取auth code
+// !第四步、获取服务器端交换用户open_id并返回平台自己对应的token来进行用户校验
+
 // 应用配置信息
 var appConfig = {
-    appId: '101169',
+    appId: '101192',
     jsArray: [
         'core.requestAuthCode',
-        'core.requestAccess',
-        'network.request'
+        'core.requestAccess'
     ],
-    getJsTicketUrl: 'https://yzb-code-storage-api-app.david-health.cn/Api/getJsApiCheckInfo',
+    getJsApiCheckInfoApiUrl: 'https://demo-api-app.yuanzhibang.com/Api/getJsApiCheckInfo',
+    getUserOpenIdAndToken: 'https://demo-api-app.yuanzhibang.com/Api/getUserinfoTokenByCode',
 };
 
 // !在yzb.ready中执行对应的操作.
 function init() {
     yzb.ready(() => {
-        getCode();
+        signIn();
     });
 }
 
-function getCode() {
-    if (window.yzb.helper.isRunInClientDesktop()) {
-        var resultDom = document.querySelector('#result');
-        resultDom.innerText = '正在获取中...';
-        getAuthCode().then(function (code) {
-            resultDom.innerText = code;
-        }).catch(function (e) {
-            resultDom.innerText = '获取code失败!';
+async function signIn() {
+    if (yzb.helper.isRunInClientDesktop()) {
+        var codeResultDom = document.querySelector('#auth_code_result');
+        var openIdResultDom = document.querySelector('#open_id_result');
+        var tokenResultDom = document.querySelector('#token_result');
+        codeResultDom.innerText = '正在获取中...';
+        try {
+            // !第一步、获取当前页面URL签名信息
+            var jsApiCheckInfo = await getJsApiCheckInfo(appConfig.getJsApiCheckInfoApiUrl);
+            jsApiCheckInfo['js_api_list'] = appConfig.jsArray;
+            jsApiCheckInfo['is_spa'] = true; // !是否为单页应用,单页面应用会对域名进行授权,域名下切换path不需要重新验证,否则验证path
+            // !第二步、进行core.config配置
+            await coreConfig(jsApiCheckInfo);
+            // !第三步、获取auth code
+            var authCode = await requestAuthCode(appConfig.appId);
+            codeResultDom.innerText = authCode;
+            // !第四步、获取服务器端交换用户open_id并返回平台自己对应的token来进行用户校验
+            var userTokenInfo = await getUserinfoByCode(appConfig.getUserOpenIdAndToken, authCode);
+            openIdResultDom.innerText = userTokenInfo.open_id;
+            tokenResultDom.innerText = userTokenInfo.token;
+        } catch (error) {
+            codeResultDom.innerText = '获取code失败!';
             alert('获取code失败');
-        });
+        }
     } else {
         alert('请在猿之棒应用内调试！');
     }
 }
 
-// !获取 auth code Promise封装
-// !第一步、获取当前页面URL签名信息
-// !第二步、进行config配置
-// !第三步、获取auth code
-function getAuthCode() {
-    return new Promise(function (resolve, reject) {
-        getJsTicketInfo().then(function (result) {
-            result['js_api_list'] = appConfig.jsArray;
-            result['is_spa'] = true;  // 是否为单页应用,单页面应用会对域名进行授权,域名下切换path不需要重新验证
-            // 进行config配置
-            config(result).then(function () {
-                // 配置成功可以进行获取access code
-                requestAuthCode(appConfig.appId).then(function (code) {
-                    // 这里返回code信息，请求交换用户信息 / token
-                    resolve(code);
-                }).catch(function (error) {
-                    reject(error);
-                });
-            }).catch(function (error) {
-                reject(error);
-            });
-        }).catch(function (error) {
-            reject(error);
-        });
-    })
-}
 
-// !获取数据js签名信息
-function getJsTicketInfo() {
-    return new Promise(function (resolve, reject) {
-        const url = window.location.href;
-        const postData = { url: url };
-        ajax(appConfig.getJsTicketUrl, postData)
-            .then(function (response) {
-                if ("2000" === response.status) {
-                    // 验证成功
-                    resolve(response.data);
-                } else {
-                    reject(response);
-                }
-            }).catch(function (error) {
-                reject(error);
-            });
-    });
-}
+// ?-----------------------js接口相关方法------------------------------
+
 
 // !进行config配置
-function config(jsTicketInfo) {
+function coreConfig(jsTicketInfo) {
     return new Promise(function (resolve, reject) {
         const config = {
             data: jsTicketInfo,
@@ -93,7 +70,7 @@ function config(jsTicketInfo) {
     });
 }
 
-// 获取auth code
+// !获取auth code
 function requestAuthCode(appId) {
     return new Promise(function (resolve, reject) {
         const config = {
@@ -110,8 +87,23 @@ function requestAuthCode(appId) {
     });
 }
 
-// ajax Promise封装
-function ajax(url, postData) {
+// ?-----------------------网络接口相关方法------------------------------
+
+// !获取数据js签名验证信息
+function getJsApiCheckInfo(apiUrl) {
+    const url = window.location.href;
+    const postData = { url: url };
+    return requestApi(apiUrl, postData);
+}
+
+// !使用code交换用户open_id
+function getUserinfoByCode(apiUrl, code) {
+    var data = { code: code };
+    return requestApi(apiUrl, data);
+}
+
+// !ajax 请求 api Promise封装
+function requestApi(url, postData) {
     return new Promise(function (resolve, reject) {
         const formData = new FormData();
         Object.keys(postData).forEach((key) => {
@@ -124,7 +116,13 @@ function ajax(url, postData) {
         ajaxObj.onreadystatechange = function () {
             if (ajaxObj.readyState === 4) {
                 if (ajaxObj.status === 200) {
-                    resolve(JSON.parse(ajaxObj.response));
+                    var apiResponse = JSON.parse(ajaxObj.responseText);
+                    if ("2000" === apiResponse.status) {
+                        // 返回成功
+                        resolve(apiResponse.data);
+                    } else {
+                        reject(apiResponse);
+                    }
                 } else {
                     reject();
                 }
